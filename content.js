94c5c06988406f9702b1f9ccc50c75e3a8a25fcc
belactriple9 <51460755+belactriple9@users.noreply.github.com>
2024-12-09 13:35:37 -0500

    Also remove grok and premium buttons

--- content.js
+++ content.js
@@ -15,6 +15,7 @@ function checkArticlesForAds() {
     });
 }
 
+
   
 // Initial check
 checkArticlesForAds();
@@ -24,6 +25,25 @@ const observer = new MutationObserver(() => {
   // Just call the function again when anything changes, probably not the most efficient way to do this
   checkArticlesForAds();
 });
+
+// wait for everything to load
+setTimeout(() => {
+    document.querySelectorAll('a').forEach(a => {
+        if(a.getAttribute('href') == '/i/grok') {
+            a.remove();
+        }
+        if(a.getAttribute('href') == '/jobs') {
+            a.remove();
+        }
+        if(a.getAttribute('href') == '/i/premium_sign_up') {
+            a.remove();
+        }
+        if(a.getAttribute('href') == '/i/verified-orgs-signup') {
+            a.remove();
+        }
+    
+    });
+}, 1000);
 
 observer.observe(document.body, { childList: true, subtree: true });
 
